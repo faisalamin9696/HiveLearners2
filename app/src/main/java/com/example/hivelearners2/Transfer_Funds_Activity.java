e9ee0f12b4965636a263547b6a833449f0c1c013
M.Faisal Amin <faisalamin9696@gmail.com>
2022-08-10 20:42:41 +0500

    Lecture 42 Update

--- app/src/main/java/com/example/hivelearners2/Transfer_Funds_Activity.java
+++ app/src/main/java/com/example/hivelearners2/Transfer_Funds_Activity.java
@@ -13,6 +13,7 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -27,6 +28,7 @@ public class Transfer_Funds_Activity extends AppCompatActivity {
 
     private FirebaseDatabase database = FirebaseDatabase.getInstance();
     private DatabaseReference sendings_ref;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +38,7 @@ public class Transfer_Funds_Activity extends AppCompatActivity {
         account_num_et = findViewById(R.id.account_num_et);
         amount_et = findViewById(R.id.amount_et);
         send_btn = findViewById(R.id.send_payment_btn);
+        firebaseAuth = FirebaseAuth.getInstance();
         progressDialog = new ProgressDialog(Transfer_Funds_Activity.this);
 
         sendings_ref = database.getReference("sendings");
@@ -48,16 +51,22 @@ public class Transfer_Funds_Activity extends AppCompatActivity {
                 Toast.makeText(this, "Some field are empty", Toast.LENGTH_SHORT).show();
                 return;
             }
+            if (firebaseAuth.getCurrentUser() != null) {
+                Toast.makeText(this, "Login Session Expired", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            String fid = firebaseAuth.getCurrentUser().getUid();
+
             HashMap<String, Object> hashMap = new HashMap<String, Object>();
             hashMap.put("account", account);
-            hashMap.put("amount", amount);
+            hashMap.put("amount", Float.parseFloat(amount));
 
             progressDialog.setCancelable(false);
             progressDialog.setMessage("Please wait...");
             progressDialog.show();
 
-            sendings_ref.push().setValue(hashMap).addOnCompleteListener(task -> {
-
+            sendings_ref.child(fid).setValue(hashMap).addOnCompleteListener(task -> {
                 if (task.isSuccessful()) {
                     if (progressDialog.isShowing())
                         progressDialog.cancel();
@@ -70,6 +79,13 @@ public class Transfer_Funds_Activity extends AppCompatActivity {
                     Toast.makeText(this, "Failed", Toast.LENGTH_SHORT).show();
                 }
                 //41 lecture end
+            });
+
+            hashMap.put("account", "My Account");
+            hashMap.put("amount", 1.21);
+
+            sendings_ref.child(fid).updateChildren(hashMap).addOnCompleteListener(task -> {
+
             });
 
 
