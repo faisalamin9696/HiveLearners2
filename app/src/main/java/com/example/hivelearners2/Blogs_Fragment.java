63d39eae64d85651d41a4fdef331dbdc1bf6b6ed
M.Faisal Amin <faisalamin9696@gmail.com>
2022-08-13 21:24:09 +0500

    Lecture 45 Update

--- app/src/main/java/com/example/hivelearners2/Blogs_Fragment.java
+++ app/src/main/java/com/example/hivelearners2/Blogs_Fragment.java
@@ -2,6 +2,7 @@ package com.example.hivelearners2;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -10,7 +11,16 @@ import android.view.ViewGroup;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class Blogs_Fragment extends Fragment implements MyAdapter.onListItemClickListener {
@@ -22,6 +32,9 @@ public class Blogs_Fragment extends Fragment implements MyAdapter.onListItemClic
     private ListView posts_lv;
     ArrayList<MyList_POJO> myList_pojos;
     MyAdapter customAdapter;
+
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference sendings_ref;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -36,6 +49,21 @@ public class Blogs_Fragment extends Fragment implements MyAdapter.onListItemClic
         myList_pojos.add(new MyList_POJO("United Kingdom", "London"));
         customAdapter = new MyAdapter(requireContext(), R.layout.my_list_item, myList_pojos);
         posts_lv.setAdapter(customAdapter);
+
+        sendings_ref = database.getReference("sendings");
+
+        sendings_ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Toast.makeText(requireContext(), snapshot.getValue().toString(), Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
         return view;
     }
